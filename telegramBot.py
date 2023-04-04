5f5eeb949492925ff41ef207de42299db8018f7c
Ben Megidish <megidish46@gmail.com>
2023-04-04 05:03:06 +0300

    commit

--- telegramBot.py
+++ telegramBot.py
@@ -1,11 +1,16 @@
 import requests
-from dotenv import load_dotenv
-import os
+# from dotenv import load_dotenv
+# import os
 
 def newMessage(message):
-    load_dotenv()
-    chatId = os.getenv('telgramChatId')
-    url = os.getenv('telgramBase_url')
+    telgramBase_url = 'https://api.telegram.org/bot6158076717:AAGA8WpqELARX4FZ4hYRavreTGy3pb3LjX8/sendMessage?chat_id='
+    telgramChatId=-1001625035427
+    telegramBotToken = '6158076717:AAGA8WpqELARX4FZ4hYRavreTGy3pb3LjX8'
+    # load_dotenv()
+    # chatId = os.getenv('telgramChatId')
+    # url = os.getenv('telgramBase_url')
+    chatId = telgramChatId
+    url = telgramBase_url
     base_url = f'{url}{chatId}&text="{message}"'
     requests.get(base_url)
   
