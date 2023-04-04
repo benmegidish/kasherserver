c290b8fa00d42b9ffa8b36e7da3b2048ebf00e72
Ben Megidish <megidish46@gmail.com>
2023-04-04 06:53:25 +0300

    commit

--- helps.py
+++ helps.py
@@ -7,12 +7,12 @@ async def myData(city):
     url= 'https://www.rest.co.il/kosher-restaurants/'+city+'/kosher/'
     print(url)
     time.sleep(3)
-    await driver.maximize_window()
-    await driver.get(url)
+    driver.maximize_window()
+    driver.get(url)
     time.sleep(2)
     content = driver.page_source.encode('utf-8').strip()
     soup = BeautifulSoup(content,"lxml")
-    numOfResualts = await soup.find('div' ,class_ = 'restaurant-info-top').text
+    numOfResualts = soup.find('div' ,class_ = 'restaurant-info-top').text
     sliceit = slice(6,10)
     num =int(numOfResualts[sliceit].replace(' ',''))
     if num>20:
@@ -23,17 +23,17 @@ async def myData(city):
     resNum = 0
     while resNum<num:
         url= 'https://www.rest.co.il/kosher-restaurants/'+city+'/kosher/page-'+str(page)+'/'
-        await driver.maximize_window()
-        await driver.get(url)
+        driver.maximize_window()
+        driver.get(url)
         time.sleep(2)
-        content = await driver.page_source.encode('utf-8').strip()
+        content = driver.page_source.encode('utf-8').strip()
         soup = BeautifulSoup(content,"lxml")
-        firstRes = await soup.find_all("div" , class_="feature-column")
+        firstRes = soup.find_all("div" , class_="feature-column")
         for res in firstRes:
-            name = await res.find("div", class_="rest-title").text
+            name = res.find("div", class_="rest-title").text
             resNum+=1
             print(name+" res num: ",resNum)
-            await telegramBot.newMessage(name)
+            telegramBot.newMessage(name)
             time.sleep(1)
         page+=1
     telegramBot.newMessage('covered all data!')
